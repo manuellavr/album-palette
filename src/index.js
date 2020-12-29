2f2bf67faf74c5d9afd38c4d5b633c903430b75b
Manuella Vieira <manuellavr7@gmail.com>
2020-12-29 19:41:46 -0300

    :children_crossing: adding pagination

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import loading from './imgs/loading.gif';
-import SearchAlbums from "./components/SearchAlbums.js";
-import {I18nextProvider} from "react-i18next";
+import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import common_br from "./translations/br/common.json";
 import common_en from "./translations/en/common.json";
 
+const SearchAlbums = lazy(() => import('./components/SearchAlbums'))
+
 i18next.init({
-	interpolation: { escapeValue: false }, 
-	lng: 'br',                              
+	interpolation: { escapeValue: false },
+	lng: 'br',
 	resources: {
 		en: {
-			common: common_en               
-        },
+			common: common_en
+		},
 		br: {
 			common: common_br
-        },
+		},
 	},
 });
 
 
 class Main extends React.Component {
 
-  render() {
-    return (
-    		<I18nextProvider i18n={i18next}>
-    		<Suspense fallback={loading}>
-    		<SearchAlbums />
-    		</Suspense>
-    		</I18nextProvider>
-    );
-  }
+	render() {
+		return (
+			<I18nextProvider i18n={i18next}>
+				<Suspense fallback={loading}>
+					<SearchAlbums />
+				</Suspense>
+			</I18nextProvider>
+		);
+	}
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
